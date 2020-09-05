de7c004647884151e862be5e298a3b7894836acd
Jyothi Sumer <mjyothisumer@gmail.com>
2020-09-05 20:59:57 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import {Header} from './components/header'
+
+import './app.scss'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Test
+        <Header></Header>
       </div>
     );
   }
---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.scss'
+import Logo from '../../assets/logo.png'
+
+export const Header = (props) => {
+  return (
+    <header>
+      <div className="wrap">
+        <div className="logo">
+          Hakunaa
+        </div>
+      </div>
+    </header>
+  )
+}
+
